8e5d62489b433e5010eea4d95f01a88ffbd9e196
abdullahalshawafi <abdullahadel.aam@gmail.com>
2021-12-21 21:18:42 +0200

    Fix assembler bug

--- WritebackStage.vhd
+++ WritebackStage.vhd
@@ -10,21 +10,20 @@ USE IEEE.numeric_std.ALL;
 -- Rd address 48:50 
 -- wb signal 51
 -- load signal 52
- 
 
 ENTITY WB_STAGE IS
     GENERIC (n : INTEGER := 16);
     PORT (
-	IM_IW_BUFFER:in std_logic_vector (52 downto 0 );
-        wb_data, Rd_data: OUT STD_LOGIC_VECTOR(n-1 DOWNTO 0);
-        Rd_address: OUT STD_LOGIC_VECTOR (2 DOWNTO 0);
+        IM_IW_BUFFER : IN STD_LOGIC_VECTOR (52 DOWNTO 0);
+        wb_data, Rd_data : OUT STD_LOGIC_VECTOR(n - 1 DOWNTO 0);
+        Rd_address : OUT STD_LOGIC_VECTOR (2 DOWNTO 0);
         WB : OUT STD_LOGIC
-	 
+
     );
 END ENTITY;
 
 ARCHITECTURE WB_STAGE_ARCH OF WB_STAGE IS
-    
+
     COMPONENT MUX2 IS
 
         PORT (
@@ -32,28 +31,18 @@ ARCHITECTURE WB_STAGE_ARCH OF WB_STAGE IS
             in1, in2 : IN STD_LOGIC_VECTOR(n - 1 DOWNTO 0);
             my_out : OUT STD_LOGIC_VECTOR(n - 1 DOWNTO 0));
     END COMPONENT;
-    
-
-   SIGNAL load_signal: STD_LOGIC;   
-   SIGNAL alu_result, read_data,wb_data_temp : STD_LOGIC_VECTOR (n - 1 DOWNTO 0);
+    SIGNAL load_signal : STD_LOGIC;
+    SIGNAL alu_result, read_data, wb_data_temp : STD_LOGIC_VECTOR (n - 1 DOWNTO 0);
 BEGIN
 
-load_signal <= IM_IW_BUFFER(52);
-alu_result <= IM_IW_BUFFER(31 downto 16);
-read_data <= IM_IW_BUFFER(15 downto 0);
+    load_signal <= IM_IW_BUFFER(52);
+    alu_result <= IM_IW_BUFFER(31 DOWNTO 16);
+    read_data <= IM_IW_BUFFER(15 DOWNTO 0);
+    WB_MUX : MUX2 PORT MAP(load_signal, alu_result, read_data, wb_data_temp);
+    wb_data <= wb_data_temp;
 
+    Rd_data <= IM_IW_BUFFER (47 DOWNTO 32);
+    Rd_address <= IM_IW_BUFFER(50 DOWNTO 48);
+    WB <= IM_IW_BUFFER(51);
 
-WB_MUX: MUX2 PORT MAP(load_signal ,alu_result ,read_data ,wb_data_temp);
-wb_data <=wb_data_temp;
-
-Rd_data <= IM_IW_BUFFER (47 downto 32);
-Rd_address <= IM_IW_BUFFER(50 downto 48);
-WB <=IM_IW_BUFFER(51);
-
-
-	 
-	            
-    
-END WB_STAGE_ARCH ;
-
-
+END WB_STAGE_ARCH;
