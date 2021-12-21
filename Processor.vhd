a6b5dc6a2c25b553de9d6d553bdfa2d484577683
Marim1611 <marimnaser813@gmail.com>
2021-12-21 21:03:53 +0200

    wb call

--- Processor.vhd
+++ Processor.vhd
@@ -52,18 +52,34 @@ ARCHITECTURE processor_arch OF processor IS
     --------------------------- Memory component ---------------------------
 
     --------------------------- Writing component ---------------------------
+    
+    COMPONENT WB_STAGE IS
+        GENERIC (n : INTEGER := 16);
+        PORT (
+            IM_IW_BUFFER:in std_logic_vector (52 downto 0 );
+                wb_data, Rd_data: OUT STD_LOGIC_VECTOR(n-1 DOWNTO 0);
+                Rd_address: OUT STD_LOGIC_VECTOR (2 DOWNTO 0);
+                WB : OUT STD_LOGIC
+            );
+    END COMPONENT;
+
     --------------------------- SIGNALS -----------------------------------
     SIGNAL IF_ID_BUFFER_FROM_FETCHING, IF_ID_BUFFER_TO_DECODING : STD_LOGIC_VECTOR(62 DOWNTO 0);
     SIGNAL ID_IE_FROM_DECODING, ID_IE_TO_EXECUTION : STD_LOGIC_VECTOR(104 DOWNTO 0);
     SIGNAL IE_IM_FROM_EXECUTION, IE_IM_TO_MEMORY : STD_LOGIC_VECTOR(74 DOWNTO 0);
     SIGNAL IM_IW_FROM_MEMORY, IM_IW_TO_WB : STD_LOGIC_VECTOR(50 DOWNTO 0);
+
+    SIGNAL  wb_data, Rd_data: STD_LOGIC_VECTOR(15 DOWNTO 0);
+    SIGNAL Rd_address: STD_LOGIC_VECTOR (2 DOWNTO 0);
+    SIGNAL  WB : STD_LOGIC;
     ------------------------------------------------------------------------
 BEGIN
     --------------------------- Fetching stage ---------------------------
+     
     --------------------------- Decoding stage ---------------------------
 
     IF_ID_BUFFER : buffer_component GENERIC MAP(n => 63) PORT MAP(clk, rst, IF_ID_BUFFER_FROM_FETCHING, IF_ID_BUFFER_TO_DECODING);
-    DECODING : decode_stage GENERIC MAP(n => 16) PORT MAP(rst, clk, IN_PORT, IF_ID_BUFFER_TO_DECODING, ID_IE_FROM_DECODING);
+    DECODING : decode_stage GENERIC MAP(n => 16) PORT MAP(rst, clk, wb_data, IF_ID_BUFFER_TO_DECODING, ID_IE_FROM_DECODING);
 
     --------------------------- Execution stage ---------------------------
     ID_IE_BUFFER : buffer_component GENERIC MAP(n => 105) PORT MAP(clk, rst, ID_IE_FROM_DECODING, ID_IE_TO_EXECUTION);
@@ -74,5 +90,6 @@ BEGIN
 
     --------------------------- Writing stage ---------------------------
     IM_IW_BUFFER : buffer_component GENERIC MAP(n => 51) PORT MAP(clk, rst, IM_IW_FROM_MEMORY, IM_IW_TO_WB);
+    WRITE_BACK: WB_STAGE GENERIC MAP(n => 16) PORT MAP(IM_IW_TO_WB, wb_data, Rd_data,Rd_address, WB );
 
 END processor_arch;
