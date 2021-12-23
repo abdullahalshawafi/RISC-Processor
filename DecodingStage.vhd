147c8e8fd738af9fbb769e088cf54df4cde2ed48
mariemzayn18 <mariamzayn18@gmail.com>
2021-12-23 23:16:17 +0200

    Yarab

--- DecodingStage.vhd
+++ DecodingStage.vhd
@@ -6,7 +6,7 @@ ENTITY decode_stage IS
     GENERIC (n : INTEGER := 32);
     PORT (
         rst, clk : IN STD_LOGIC;
-        -- IN_PORT : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+        WB_address : STD_LOGIC_VECTOR(2 DOWNTO 0);
         WB : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
         IF_ID_BUFFER : IN STD_LOGIC_VECTOR(80 DOWNTO 0);
         pc_en : OUT STD_LOGIC;
@@ -75,22 +75,17 @@ BEGIN
     --------------------------------------------------------------
     CU : CONTROL_UNIT PORT MAP('0', op_code, pc_write, inst_type, flush, set_carry, branch, alu_src, Rs_en, Rt_en, mem_read, mem_write, interrupt_en, stack, load, reg_write, in_en, out_en, alu_op, flag_en, stack_op);
 
-    -----------------------------------------------------------------    
-    -- WD_mux : MUX2 GENERIC MAP(n => 16) PORT MAP(in_en, WB, IN_PORT, Wd);
-
     -----------------------------------------------------------------
 
-    Rx : register_file PORT MAP(clk, rst, reg_write, Rs_address, Rt_address, Rd_address, Wd, Rs_data, Rt_data);
+    Rx : register_file PORT MAP(clk, rst, reg_write, Rs_address, Rt_address, WB_address, WB, Rs_data, Rt_data);
 
     -----------------------------------------------------------------
     ID_IE_BUFFER(122 DOWNTO 107) <= IF_ID_BUFFER(80 DOWNTO 65); -- INPUT PORT 
     ID_IE_BUFFER(106 DOWNTO 96) <= in_en & load & reg_write & alu_op & alu_src & flag_en & set_carry;
-    ID_IE_BUFFER(95 DOWNTO 64) <= (OTHERS => '0');
+    ID_IE_BUFFER(95 DOWNTO 64) <= IF_ID_BUFFER(49 DOWNTO 34) & op_code & Rs_address & Rt_address & Rd_address & "00";
     ID_IE_BUFFER(63 DOWNTO 48) <= Rt_data;
     ID_IE_BUFFER(47 DOWNTO 32) <= Rs_data;
     ID_IE_BUFFER(31 DOWNTO 0) <= IF_ID_BUFFER(31 DOWNTO 0);
-    -- ID_IE_BUFFER[96 SETC ,  97,98,99 FlagEn znc , 100 AluSrc , 101:103 AluOP i will assume for now it is 3 bits] 
-    -- ID_IE_BUFFER(64:95) instruction 32 bit => 80:95 imm. value
     -----------------------------------------------------------------
 
     pc_en <= pc_write;
