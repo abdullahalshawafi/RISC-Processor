ae0d811579735735e9abca33a94279d7a8e9f4a7
Marim1611 <marimnaser813@gmail.com>
2022-01-08 17:16:03 +0200

    un do :((

--- Processor.vhd
+++ Processor.vhd
@@ -53,9 +53,7 @@ ARCHITECTURE PROCESSOR OF PROCESSOR IS
             exception : IN STD_LOGIC;
             pc_en : OUT STD_LOGIC := '1';
             inst_type : OUT STD_LOGIC := '0';
-            ID_IE_BUFFER : OUT STD_LOGIC_VECTOR(131 DOWNTO 0);
-            Z_flag, N_flag, C_flag : IN STD_LOGIC;
-            will_branch : OUT STD_LOGIC
+            ID_IE_BUFFER : OUT STD_LOGIC_VECTOR(131 DOWNTO 0)
         );
     END COMPONENT;
 
@@ -64,14 +62,15 @@ ARCHITECTURE PROCESSOR OF PROCESSOR IS
     COMPONENT EX_STAGE IS
         GENERIC (n : INTEGER := 16);
         PORT (
+
             ID_IE_BUFFER : IN STD_LOGIC_VECTOR (131 DOWNTO 0);
             IE_IM_BUFFER : OUT STD_LOGIC_VECTOR (76 DOWNTO 0);
             Rd_M_address, Rd_W_address : IN STD_LOGIC_VECTOR (2 DOWNTO 0);
             Rd_M_data, Rd_W_data : IN STD_LOGIC_VECTOR (n - 1 DOWNTO 0);
             clk, rst, WB_M, WB_W : IN STD_LOGIC;
+            will_branch : OUT STD_LOGIC;
             target : OUT STD_LOGIC_VECTOR (31 DOWNTO 0);
-            exception : IN STD_LOGIC;
-            Z_flag, N_flag, C_flag : OUT STD_LOGIC
+            exception : IN STD_LOGIC
         );
 
     END COMPONENT;
@@ -124,15 +123,11 @@ BEGIN
     IF_ID_BUFFER : buffer_component GENERIC MAP(n => 81) PORT MAP(clk, rst, '1', IF_ID_BUFFER_FROM_FETCHING, IF_ID_BUFFER_TO_DECODING);
 
     -- check data of buffers for HDU    
-    DECODING : DECODING_STAGE GENERIC MAP(
-        n => 16) PORT MAP(rst, clk, Rd_address, wb_data, WB, IF_ID_BUFFER_TO_DECODING, IF_ID_BUFFER_TO_DECODING(58 DOWNTO 56), IF_ID_BUFFER_TO_DECODING(55 DOWNTO 53), ID_IE_TO_EXECUTION(68 DOWNTO 66), ID_IE_TO_EXECUTION(124), Exception, pc_write, instType, ID_IE_FROM_DECODING
-        , Z_flag, N_flag, C_flag, WILL_BRANCH);
+    DECODING : DECODING_STAGE GENERIC MAP(n => 16) PORT MAP(rst, clk, Rd_address, wb_data, WB, IF_ID_BUFFER_TO_DECODING, IF_ID_BUFFER_TO_DECODING(58 DOWNTO 56), IF_ID_BUFFER_TO_DECODING(55 DOWNTO 53), ID_IE_TO_EXECUTION(68 DOWNTO 66), ID_IE_TO_EXECUTION(124), Exception, pc_write, instType, ID_IE_FROM_DECODING);
 
     --------------------------- Execution Stage ---------------------------
     ID_IE_BUFFER : buffer_component GENERIC MAP(n => 132) PORT MAP(clk, rst, '1', ID_IE_FROM_DECODING, ID_IE_TO_EXECUTION);
-    EXECUTION : EX_STAGE GENERIC MAP(
-        n => 16) PORT MAP(ID_IE_TO_EXECUTION, IE_IM_FROM_EXECUTION, IE_IM_TO_MEMORY(66 DOWNTO 64), IM_IW_TO_WB(50 DOWNTO 48), IE_IM_TO_MEMORY(47 DOWNTO 32), wb_data, clk, rst, IE_IM_TO_MEMORY(74), WB, TARGET,
-        Exception, Z_flag, N_flag, C_flag);
+    EXECUTION : EX_STAGE GENERIC MAP(n => 16) PORT MAP(ID_IE_TO_EXECUTION, IE_IM_FROM_EXECUTION, IE_IM_TO_MEMORY(66 DOWNTO 64), IM_IW_TO_WB(50 DOWNTO 48), IE_IM_TO_MEMORY(47 DOWNTO 32), wb_data, clk, rst, IE_IM_TO_MEMORY(74), WB, WILL_BRANCH, TARGET, Exception);
 
     --------------------------- Memory Stage ---------------------------
     IE_IM_BUFFER : buffer_component GENERIC MAP(n => 77) PORT MAP(clk, rst, '1', IE_IM_FROM_EXECUTION, IE_IM_TO_MEMORY);
